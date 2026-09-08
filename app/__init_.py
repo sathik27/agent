6e26c3c3a30a10ff05fe62ef29885431ddca9def
sathik27 <sathikbizz12@gmail.com>
2026-09-08 10:00:18 +0530

    Initialize Flask app with YouTube blueprint
    Fix imports and define home and create_app functions.

--- app/__init_.py
+++ app/__init_.py
@@ -1,1 +1,18 @@
+import os, urlib.parse.request,render_template
+from app.youtube import youtube_bp
 
+gemini_api_key = "gemini_api_key";
+
+def home():
+   return render_template (" index.html")
+
+def create_app():
+
+  app = flask(_name_)
+  app.register_blueprint(youtube_bp, url_prefix="/youtube")
+  @app.route("/html")
+   def html():
+       return render_template("index.html")
+
+return app;
+     
